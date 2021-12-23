404486b48307cab7632d444cd63ec9d9d4c8fc37
vinit <vinitsxn@gmail.com>
2021-12-23 10:09:22 +0530

    Added: Daily Wage Employee Functionality Added

--- src/employewage/EmployeWageBuilder.java
+++ src/employewage/EmployeWageBuilder.java
@@ -5,14 +5,19 @@ public class EmployeWageBuilder {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Builder");
         int IS_PRESENT=1;
+        int FULL_RATE_PER_HOUR=20;
+        int empHours=0;
+        int empWage=0;
         Random random=new Random();
         int empCheck= random.nextInt( 2);
         if(empCheck==IS_PRESENT) {
+            empHours=8;
             System.out.println("Employee is Present");
         }
         else{
             System.out.println("Employee is Absent");
             }
-
+        empWage=empHours*FULL_RATE_PER_HOUR;
+        System.out.println("The daily wage of an employee is "+ empWage);
     }
 }
