fe0958435057ed5d389f5759a346ae480895d61a
vinit <vinitsxn@gmail.com>
2021-12-24 09:27:35 +0530

    Modified: If else replaced with switch case

--- src/employewage/EmployeWageBuilder.java
+++ src/employewage/EmployeWageBuilder.java
@@ -2,27 +2,27 @@ package employewage;
 import java.util.Random;
 
 public class EmployeWageBuilder {
+    public static final int IS_FULL_TIME=1;
+    public static final int EMP_RATE_PER_HOUR=20;
+    public static final int IS_PART_TIME=2;
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Builder");
-        int IS_PRESENT=1;
-        int FULL_RATE_PER_HOUR=20;
-        int IS_PART_TIME=2;
         int empHours=0;
         int empWage=0;
         Random random=new Random();
         int empCheck= random.nextInt( 3);
-        if(empCheck==IS_PRESENT) {
-            empHours=8;
-            System.out.println("Employee is Present");
+        switch(empCheck){
+            case IS_FULL_TIME:
+                empHours=8;
+                break;
+            case IS_PART_TIME:
+                empHours=4;
+                break;
+            default:
+                empHours=0;
+                break;
         }
-        else if (empCheck==IS_PART_TIME){
-            empHours=4;
-            System.out.println("Part Time Employee");
-        }
-        else{
-            System.out.println("Employee is Absent");
-            }
-        empWage=empHours*FULL_RATE_PER_HOUR;
+        empWage=empHours*EMP_RATE_PER_HOUR;
         System.out.println("The daily wage of an employee is "+ empWage);
     }
 }
