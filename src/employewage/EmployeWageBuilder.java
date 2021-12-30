d625939bd97217be77831dc21ce54117be6e0675
vinit <vinitsxn@gmail.com>
2021-12-30 16:03:52 +0530

    Modified: Procedural Programming replaced to Object Oriented Programming

--- src/employewage/EmployeWageBuilder.java
+++ src/employewage/EmployeWageBuilder.java
@@ -1,41 +1,63 @@
 package employewage;
 import java.util.Random;
 
-public class EmployeWageBuilder {
-    public static final int IS_FULL_TIME=1;
-    public static final int EMP_RATE_PER_HOUR=20;
-    public static final int IS_PART_TIME=2;
-    public static final int NO_OF_WORKING_DAYS=20;
-    public static final int TOTAL_WORKING_HOURS=100;
-    public static void main(String args[]) {
-        System.out.println("Welcome to Employee Wage Builder");
-        int empHours=0;
-        int empWage=0;
-        int totalEmployeeEage=0;
-        int totalEmpHours=0;
-        int totalWorkingDays=0;
-        while(totalEmpHours<TOTAL_WORKING_HOURS && totalWorkingDays<NO_OF_WORKING_DAYS){
-            totalWorkingDays++;
+class Employee{
+    //variables
+    private int empHours,empWage,totalEmployeeWage,totalEmpHours,totalWorkingDays;
+    private static final int IS_FULL_TIME=1;
+    private static final int EMP_RATE_PER_HOUR=20;
+    private static final int IS_PART_TIME=2;
+    private static final int NO_OF_WORKING_DAYS=20;
+    private static final int TOTAL_WORKING_HOURS=100;
+    //constructor
+    Employee(int empHours,int empWage,int totalEmployeeWage,int totalEmpHours,int totalWorkingDays){
+        this.empHours=empHours;
+        this.empWage=empWage;
+        this.totalEmployeeWage=totalEmployeeWage;
+        this.totalEmpHours=totalEmpHours;
+        this.totalWorkingDays=totalWorkingDays;
+    }
+    //method to find Employee Wage
+    public void employeeWageCalculator(Employee employee){
+        while(employee.totalEmpHours<TOTAL_WORKING_HOURS && employee.totalWorkingDays<NO_OF_WORKING_DAYS){
+            employee.totalWorkingDays++;
             Random random=new Random();
             int empCheck= random.nextInt( 3);
             switch(empCheck){
                 case IS_FULL_TIME:
-                    empHours=8;
+                    employee.empHours=8;
                     break;
                 case IS_PART_TIME:
-                    empHours=4;
+                    employee.empHours=4;
                     break;
                 default:
-                    empHours=0;
+                    employee.empHours=0;
                     break;
             }
             if(totalEmpHours+empHours>100 && empHours>(100-totalEmpHours)){
                 empHours=100-totalEmpHours;
             }
-            totalEmpHours=totalEmpHours+empHours;
-            empWage=empHours*EMP_RATE_PER_HOUR;
-            totalEmployeeEage=totalEmployeeEage+empWage;
+            employee.totalEmpHours=employee.totalEmpHours+ employee.empHours;
+            employee.empWage=employee.empHours*EMP_RATE_PER_HOUR;
+            employee.totalEmployeeWage= employee.totalEmployeeWage+employee.empWage;
         }
-        System.out.println("The total employee wage of an employee for "+totalEmpHours+" hours for "+totalWorkingDays+" days is: "+ totalEmployeeEage);
+        System.out.println("The total employee wage of an employee for "+employee.totalEmpHours+" hours or "+employee.totalWorkingDays+" days is: "+ employee.totalEmployeeWage);
     }
 }
+public class EmployeWageBuilder {
+
+    public static void main(String args[]) {
+        System.out.println("Welcome to Employee Wage Builder");
+        //variable declaration
+        int empHours=0;
+        int empWage=0;
+        int totalEmployeeWage=0;
+        int totalEmpHours=0;
+        int totalWorkingDays=0;
+        //object creation
+        Employee employee=new Employee(empHours,empWage,totalEmployeeWage,totalEmpHours,totalWorkingDays);
+        //calculating Wages
+        employee.employeeWageCalculator(employee);
+    }
+}
+
