722c91938f53b46afcaa52859ae471399ad66dc3
vinit <vinitsxn@gmail.com>
2021-12-30 15:54:04 +0530

    Added: MaxWorking Hours Check functionality added

--- src/employewage/EmployeWageBuilder.java
+++ src/employewage/EmployeWageBuilder.java
@@ -6,6 +6,7 @@ public class EmployeWageBuilder {
     public static final int EMP_RATE_PER_HOUR=20;
     public static final int IS_PART_TIME=2;
     public static final int NO_OF_WORKING_DAYS=20;
+    public static final int TOTAL_WORKING_HOURS=100;
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Builder");
         int empHours=0;
@@ -13,7 +14,7 @@ public class EmployeWageBuilder {
         int totalEmployeeEage=0;
         int totalEmpHours=0;
         int totalWorkingDays=0;
-        while(totalWorkingDays<NO_OF_WORKING_DAYS){
+        while(totalEmpHours<TOTAL_WORKING_HOURS && totalWorkingDays<NO_OF_WORKING_DAYS){
             totalWorkingDays++;
             Random random=new Random();
             int empCheck= random.nextInt( 3);
@@ -28,10 +29,13 @@ public class EmployeWageBuilder {
                     empHours=0;
                     break;
             }
+            if(totalEmpHours+empHours>100 && empHours>(100-totalEmpHours)){
+                empHours=100-totalEmpHours;
+            }
             totalEmpHours=totalEmpHours+empHours;
             empWage=empHours*EMP_RATE_PER_HOUR;
             totalEmployeeEage=totalEmployeeEage+empWage;
         }
-        System.out.println("The total employee wage of an employee for "+totalWorkingDays+" days is: "+ totalEmployeeEage);
+        System.out.println("The total employee wage of an employee for "+totalEmpHours+" hours for "+totalWorkingDays+" days is: "+ totalEmployeeEage);
     }
 }
